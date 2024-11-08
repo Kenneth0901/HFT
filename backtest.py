16e04f8b73f2dc82a1e5b5fa2ee9071f6f0fef33
Kenneth <zhanxuanyou@126.com>
2024-11-08 19:22:31 +0800

    event driven backtest

--- backtest.py
+++ backtest.py
@@ -4,14 +4,9 @@ import matplotlib.pyplot as plt
 
 
 def Sharpe(pnl:pd.Series)->float:
-    sharpe = pnl.mean() / pnl.std()
+    sharpe = np.sqrt(365*24*60) * (pnl - 3.8e-8).mean() / (pnl - 3.8e-8).std()
     return sharpe
 
-def calculate_Cr(df:pd.DataFrame, window:int)->pd.Series:
-    df['return'] = df['close'].astype(float).diff(1).shift(-1) / df['close'].astype(float)
-    reversed_data = df['return'][::-1]
-    Cr = (reversed_data).rolling(window).sum()[::-1]
-    return Cr
 
 def generate_signal(df:pd.DataFrame)->pd.Series:
     N = 20
@@ -20,7 +15,7 @@ def generate_signal(df:pd.DataFrame)->pd.Series:
     Sd = df['close'].rolling(N).std()
     Mb = Ma + k*Sd
     Dn = Ma - k*Sd
-    raw_Signal = (df['close'] < Dn + 0.5*Sd) * (df['close'] > Dn - 0.5*Sd) * 0.1
+    raw_Signal = (df['close'] < Dn ) * 0.1
     
     # raw_Signal = -(df['close'].astype(float)/df['open'].astype(float) -1)*100
     # raw_Signal = raw_Signal *(raw_Signal>0)
@@ -34,10 +29,10 @@ def position_restrict(Signal:pd.Series, window):
     return new_Signal
     
 
-def calculate_pnl(Position:pd.Series, Cumulative_Return:pd.Series):
-    Cumulative_Return = Cumulative_Return.dropna()
-    Position = Position.reindex_like(Cumulative_Return)
-    pnl = Position * Cumulative_Return
+def calculate_pnl(Position:pd.Series, Return:pd.Series):
+    Return = Return.dropna()
+    Position = Position.reindex_like(Return)
+    pnl = Position * Return
     return pnl
     
 def calculate_cost(Position:pd.Series):
@@ -45,12 +40,13 @@ def calculate_cost(Position:pd.Series):
     return cost
 
 def main():
-    df = pd.read_parquet('2024.parquet')[:]
-    window = 10
-    Cr = calculate_Cr(df, 5)
+    df = pd.read_parquet('2024.parquet')[:1000]
+    window = 5
+
+    df['return'] = df['close'].astype(float).diff(1).shift(-1) / df['close'].astype(float)
     row_Signal = generate_signal(df)
     Signal = position_restrict(row_Signal, window)
-    pnl = calculate_pnl(Signal, Cr)
+    pnl = calculate_pnl(Signal, df['return'])
     pnl.index = pd.to_datetime(pnl.index, unit='ms')
     Position = (Signal - Signal.shift(window).fillna(0)).cumsum()
     Position.index = pd.to_datetime(Position.index, unit='ms')
@@ -61,8 +57,8 @@ def main():
     plt.figure(figsize=[32, 16])
     p1 = plt.subplot(211)
     p2 = plt.subplot(212)
-    p1.plot(pnl.fillna(0).cumsum(), label=f"PNL, Sharpe: {Sharpe(pnl)}")
-    p1.plot(pnl_net.fillna(0).cumsum(), label=f"PNL_Net, Sharpe: {Sharpe(pnl_net)}")
+    p1.plot((1+pnl.fillna(0)).cumprod(), label=f"PNL, Sharpe: {Sharpe(pnl)}")
+    p1.plot((1+pnl_net.fillna(0)).cumprod(), label=f"PNL_Net, Sharpe: {Sharpe(pnl_net)}")
     p1.grid(linestyle='--')
     p1.legend(loc='upper left')
     p2.plot(Position, label=f"PNL_Net")
